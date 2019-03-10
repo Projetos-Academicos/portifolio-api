13e5429905711d7cd922f233e0b7990ea89bd61a
Ricardo Lima <ricardoccofg@gmail.com>
2019-03-10 00:31:03 -0300

    subir primeira versão

--- src/main/java/com/ricardolima/site/controllers/EmailController.java
+++ src/main/java/com/ricardolima/site/controllers/EmailController.java
@@ -1,6 +1,7 @@
 package com.ricardolima.site.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class EmailController {
 	@Autowired
 	private EmailService service;
 
+	@PostMapping("/enviar")
 	public Retorno enviarEmail(Email email) {
 		return this.service.sendEmail(email);
 	}
